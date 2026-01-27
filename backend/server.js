dd0086f9aecb4ffad506d6ecc535a19ec8cb452d
Subraj-Kumar <kumarsubraj97@gmail.com>
2026-01-27 18:05:11 +0530

    Backend and MongoDB setup done

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", message: "CampusConnect backend running" });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
